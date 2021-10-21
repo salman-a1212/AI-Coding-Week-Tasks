1826f53c413aa209421c746df42514b972450ec1
salman-a1212 <82266019+salman-a1212@users.noreply.github.com>
2021-10-21 10:04:20 +0500

    Update index.js

--- docs/index.js
+++ docs/index.js
@@ -1,6 +1,7 @@
 const dayTime = new Date();
 
-const currentDay = new Date();
+const currentDate = new Date();
+console.log("current date", currentDate);
 
 const weekday = new Array(7);
 weekday[0] = "Sunday";
@@ -16,3 +17,30 @@ let day = weekday[dayTime.getDay()];
 document.getElementById("current-day").innerText = `Today is: ${day},`;
 
 console.log("day", day);
+
+var today = new Date();
+var time =
+  today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+
+function timer() {
+  var currentTime = new Date();
+  var hours = currentTime.getHours();
+  var minutes = currentTime.getMinutes();
+  var sec = currentTime.getSeconds();
+  if (minutes < 10) {
+    minutes = "0" + minutes;
+  }
+  if (sec < 10) {
+    sec = "0" + sec;
+  }
+  var t_str = hours + ":" + minutes + ":" + sec + " ";
+  if (hours > 11) {
+    t_str += "PM";
+  } else {
+    t_str += "AM";
+  }
+  document.getElementById("current-time").innerHTML = t_str;
+  setTimeout(timer, 1000);
+}
+
+timer();
