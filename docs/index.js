c03222610baad008bc9fe3231d9ff7a0e9f6bcbf
salman-a1212 <82266019+salman-a1212@users.noreply.github.com>
2021-10-23 17:34:13 +0500

    Update index.js

--- docs/index.js
+++ docs/index.js
@@ -128,9 +128,10 @@ lpBtn.onclick = function checkLeapYear() {
 let sunBtn = document.getElementById("sun-btn");
 sunBtn.onclick = function sunLookUp() {
   let input = Number(document.getElementById("inptyr").value);
+  console.log("input", input);
   let sunRndr = document.getElementById("sunrndr");
   var year = input;
-  if (year === 2014 && year <= 2050) {
+  if ((year >= "2014", year <= "2050")) {
     var date = new Date(year, 0, 1);
     if (date.getDay() === 0) {
       sunRndr.innerText = `1st January is being a Sunday in the year ${year}`;
