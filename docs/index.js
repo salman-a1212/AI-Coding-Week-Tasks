ce84085314084194cdbd29671350c42c43e4432a
salman-a1212 <82266019+salman-a1212@users.noreply.github.com>
2021-10-23 17:28:42 +0500

    Update index.js

--- docs/index.js
+++ docs/index.js
@@ -130,7 +130,7 @@ sunBtn.onclick = function sunLookUp() {
   let input = document.getElementById("inptyr").value;
   let sunRndr = document.getElementById("sunrndr");
   var year = input;
-  if ((year === "2014", year <= "2050")) {
+  if (year === "2014" && year <= "2050") {
     var date = new Date(year, 0, 1);
     if (date.getDay() === 0) {
       sunRndr.innerText = `1st January is being a Sunday in the year ${year}`;
